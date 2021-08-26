932f224ce83d9668e20cfa5ac74680126d82a982
cosmin ciolacu <cosminciolacu99@gmail.com>
2021-08-26 11:05:00 +0300

    fix code 2

--- client/src/pages/Editor.tsx
+++ client/src/pages/Editor.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import io, { Socket } from "socket.io-client";
 import { useParams } from "react-router-dom";
@@ -11,7 +11,7 @@ function CodeEditor() {
   const { id } = useParams<EditorParams>();
   const [code, setCode] = useState<string>("// some comment");
   const [socket, setSocket] = useState<Socket | null>(null);
-  const socketRef = useRef<Socket>();
+  const [showEditor, setShowEditor] = useState<boolean>(false);
   useEffect(() => {
     /* socketRef.current = io("http://localhost:5000");
     socketRef.current.emit("join room", id); */
@@ -19,30 +19,38 @@ function CodeEditor() {
   }, []);
 
   useEffect(() => {
-    socket?.emit("join", id);
     socket?.on("recieve-code", (code) => {
-      console.log("recieving code...");
-      setCode(code);
+      setCode(code as string);
     });
-  }, []);
+    return () => {
+      socket?.emit("leave", id);
+    };
+  }, [socket]);
+  const joinEditor = () => {
+    socket?.emit("join", id);
+    setShowEditor((prev) => !prev);
+  };
 
   return (
     <div>
       <h1>CodeEditor</h1>
-      <Editor
-        height="90vh"
-        theme="vs-dark"
-        defaultLanguage="java"
-        defaultValue={code}
-        onChange={(value, ev) => {
-          console.log(value);
-          setCode(value as string);
-          socket?.emit("code-change", {
-            room: id,
-            code: value,
-          });
-        }}
-      />
+      <button onClick={() => joinEditor()}>START</button>
+      {showEditor && (
+        <Editor
+          height="90vh"
+          theme="vs-dark"
+          defaultLanguage="java"
+          defaultValue={code}
+          value={code}
+          onChange={(value, ev) => {
+            setCode(value as string);
+            socket?.emit("code-change", {
+              room: id,
+              code: value,
+            });
+          }}
+        />
+      )}
     </div>
   );
 }
