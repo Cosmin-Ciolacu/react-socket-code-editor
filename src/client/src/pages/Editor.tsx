3bc40c26cdaf3817b65ab3ad09850535bcaf7488
cosmin ciolacu <cosminciolacu99@gmail.com>
2021-09-22 16:17:47 +0300

    add compiler

--- src/client/src/pages/Editor.tsx
+++ src/client/src/pages/Editor.tsx
@@ -2,20 +2,26 @@ import { useState, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import io, { Socket } from "socket.io-client";
 import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 
 interface EditorParams {
   id: string;
 }
 
+const SOCKET_URL: string = process.env.REACT_APP_SOCKET_URL || "/";
+
 function CodeEditor() {
   const { id } = useParams<EditorParams>();
   const [code, setCode] = useState<any>("// some comment");
+  const [language, setLanguage] = useState<string>("javascript");
+  const [output, setOutput] = useState<string>("");
+  const [err, setErr] = useState("");
   const [socket, setSocket] = useState<Socket | null>(null);
   const [showEditor, setShowEditor] = useState<boolean>(false);
   useEffect(() => {
     /* socketRef.current = io("http://localhost:5000");
     socketRef.current.emit("join room", id); */
-    setSocket(io("/"));
+    setSocket(io(SOCKET_URL));
   }, []);
 
   useEffect(() => {
@@ -24,6 +30,8 @@ function CodeEditor() {
       console.log("code recieved", code);
       setCode(code);
     });
+    socket?.on("recieve-output", (output) => setOutput(output));
+    socket?.on("recieve-language", (language) => setLanguage(language));
     /* return () => {
       socket?.emit("leave", id);
     }; */
@@ -32,6 +40,20 @@ function CodeEditor() {
     socket?.emit("join", id);
     setShowEditor((prev) => !prev);
   };
+  const compile = async () => {
+    const res = await axios.post("/compiler", {
+      code,
+      language,
+    });
+    const data = res.data;
+    console.log(data);
+    setOutput(data.data.output);
+    setErr(data.data.err);
+    socket?.emit("send-output", {
+      room: id,
+      output: data.data.output,
+    });
+  };
 
   return (
     <div>
@@ -39,11 +61,32 @@ function CodeEditor() {
         <h1>CodeEditor</h1>
       </Link>
       <button onClick={() => joinEditor()}>START</button>
+      <div>
+        <select
+          value={language}
+          onChange={(e) => {
+            setLanguage(e.target.value);
+            socket?.emit("language-change", {
+              room: id,
+              language: e.target.value,
+            });
+          }}
+        >
+          <option value="javascript">javascript</option>
+          <option value="c++">c++</option>
+          <option value="java">java</option>
+          <option value="python">python</option>
+        </select>
+        <button onClick={() => compile()}>Compileaza codul...</button>
+        {output && <pre> output: {output}</pre>}
+        {err && <pre>err: {output}</pre>}
+      </div>
       {showEditor && (
         <Editor
           height="90vh"
           theme="vs-dark"
-          defaultLanguage="java"
+          defaultLanguage={language}
+          language={language}
           defaultValue={code}
           value={code}
           onChange={(value, ev) => {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.static("client/build"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 const httpServer = require("http").createServer(app);
 const options = {
   /* ... */
@@ -11,7 +13,61 @@ const options = {
 };
 const io = require("socket.io")(httpServer, options);
 const dotenv = require("dotenv");
+const { promisify } = require("util");
+const { exec } = require("child_process");
+const { writeFileSync, unlinkSync } = require("fs");
+const execute = promisify(exec);
 dotenv.config();
+app.post("/compiler", async (request, response) => {
+  const { language, code } = request.body;
+  console.log(language, code);
+
+  let output = null;
+  let compileError = null;
+  switch (language) {
+    case "javascript": {
+      const filename = `./${Math.floor(Math.random() * 1000)}.js`;
+      writeFileSync(filename, code);
+      const data = await execute(`node ${filename}`);
+      console.log(data);
+      output = data.stdout;
+      compileError = data.stderr;
+      unlinkSync(filename);
+      break;
+    }
+    case "python": {
+      const filename = `./${Math.floor(Math.random() * 1000)}.py`;
+      writeFileSync(filename, code);
+      const data = await execute(`python ${filename}`);
+      console.log(data);
+      output = data.stdout;
+      compileError = data.stderr;
+      unlinkSync(filename);
+      break;
+    }
+    case "java": {
+      const filename = `./${Math.floor(Math.random() * 1000)}.java`;
+      writeFileSync(filename, code);
+      const data = await execute(`javac ${filename} && java ${filename}`);
+      console.log(data);
+      output = data.stdout;
+      compileError = data.stderr;
+      unlinkSync(filename);
+      break;
+    }
+    default: {
+      output = "erroare";
+      break;
+    }
+  }
+
+  return response.json({
+    data: {
+      output,
+      err: compileError,
+    },
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -25,6 +81,15 @@ io.on("connection", (socket) => {
   socket.on("code-change", (data) => {
     //console.log(data);
     io.to(data.room).emit("recieve-code", data.code);
+  });
+
+  socket.on("language-change", (data) => {
+    //console.log(data);
+    io.to(data.room).emit("recieve-language", data.language);
+  });
+
+  socket.on("send-output", (data) => {
+    io.to(data.room).emit("recieve-output", data.output);
   });
 
   socket.on("leave", (room) => {
